8913904ad898487fbc89a8d9f0263ba6141fb3ca
nexzanavod <aanavod@gmail.com>
2025-05-29 16:00:30 +0530

    Initial commit: Add Shopify-MyPOS sync middleware (MaiaBridge)

--- src/services/getMyPOSStockData.js
+++ src/services/getMyPOSStockData.js
@@ -5,11 +5,11 @@ const { callApi } = require('../config/apiClient');
 async function getMyPOSStockData() {
 
       //   This is a mockup of the API response
-      const data = await fs.readFile('./test.json', 'utf-8');
-      const response = JSON.parse(data);
+      // const data = await fs.readFile('./test.json', 'utf-8');
+      // const response = JSON.parse(data);
 
 
-    // const response = await callApi('gettotalstockinhand', {});
+    const response = await callApi('gettotalstockinhand', {});
     return response?.Data?.Products || [];
   }
 
