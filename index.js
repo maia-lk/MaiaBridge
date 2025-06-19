218f5d3224424301361144e38c5db33a2f065401
nexzanavod <aanavod@gmail.com>
2025-06-19 06:06:18 +0530

    Apply maia webhook

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const shopifRoutes = require('./src/routes/shopifRoutes');
 const myposRoutes = require('./src/routes/myposRoutes');
 const authRoutes = require('./src/routes/authRoutes');
 const shopifyWebhookRoutes = require('./src/routes/shopifyWebhookRoutes');
-const cron = require('node-cron');
-const { syncMyPOSStockToShopify } = require('./src/controllers/shopifyController');
 
 dotenv.config();
 const app = express();
@@ -22,11 +20,6 @@ app.use('/mypos', myposRoutes);
 app.use('/auth', authRoutes);
 app.use('/shopify-webhook', shopifyWebhookRoutes);
 
-// Cron job: run syncMyPOSStockToShopify every 30 minutes
-cron.schedule('*/30 * * * *', () => {
-  console.log('Running stock sync...');
-  syncMyPOSStockToShopify();
-});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
